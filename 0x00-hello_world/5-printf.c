543db85c04b002ff74790d932c9268654319b519
TORGBORFRANCISCA <fhrannie92@gmail.com>
2024-06-20 12:50:35 -0700

    q5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is a printf function
+ *
+ * Return: 0 when it works
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
