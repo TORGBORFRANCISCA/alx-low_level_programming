fb43cde7da4e962d6b4a467a6ec86abf0b4c8643
TORGBORFRANCISCA <fhrannie92@gmail.com>
2024-07-22 11:03:53 -0700

    q0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - initializes the struct
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: name of dog's owner
+ *
+ * Description: learning struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
