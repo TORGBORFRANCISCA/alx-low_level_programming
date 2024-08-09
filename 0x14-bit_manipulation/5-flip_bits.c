929c235e1906b2d81fe45544102c8c19eb551777
TORGBORFRANCISCA <fhrannie92@gmail.com>
2024-08-09 02:33:13 -0700

    q5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,24 +1,23 @@
 #include "main.h"
+
 /**
- * flip_bits - returns the differeces about digits between 2 numbers.
- * @n: the input number1
- * @m: input number 2
- * Return: the diferences
- */
+*flip_bits - gets the number of bits to flip to get from n to m
+*@n: first number
+*@m: last number
+*
+*Return: the number of flipped bits
+*/
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int bit_diff, mask = 0;
-	int i, digit;
-	unsigned int differences = 0;
+	unsigned long int flipped = n ^ m;
+	int count = 0;
 
-	bit_diff = n ^ m;/*1 if bit_n != bit_m (XOR)*/
-
-	for (i = 63; i >= 0; i--)
+	while (flipped)
 	{
-		mask = 1 << i;/*Creating a mask for extract the digit*/
-		digit = (bit_diff & mask) >> i;/*Extract the digit and push to 0 or 1*/
-		if (digit == 1)
-			differences++;
+		if (flipped & 1)
+			count++;
+		flipped >>= 1;
 	}
-	return (differences);
+	return (count);
 }
