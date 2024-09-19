721b529085330c6765b80f36a94fc53c54b41776
TORGBORFRANCISCA <fhrannie92@gmail.com>
2024-09-19 05:39:52 -0700

    q7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,54 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index -  insert a node in a idx position
- * @h: pointer to head in function
- * @idx: index or position to be added
- * @n: nodes value
- * Return: nth node data
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int nodes = 0;
-	dlistint_t *node_index = *h;
-	dlistint_t *new_node, *node_after;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (h == NULL)
-		return (NULL);
-
-	if (idx != 0)
-	{
-		while (nodes < idx - 1 && node_index != NULL)
-		{
-			node_index = node_index->next;
-			nodes++;
-		}
-		if (node_index == NULL)
-			return (NULL);
-	}
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
+	new = NULL;
 	if (idx == 0)
-	{
-		node_after = *h;
-		*h = new_node;
-		new_node->prev = NULL;
-	}
+		new = add_dnodeint(h, n);
 	else
 	{
-		new_node->prev = node_index;
-		node_after = node_index->next;
-		node_index->next = new_node;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	new_node->next = node_after;
-	if (new_node != NULL)
-		new_node->next->prev = new_node;
-	return (new_node);
+
+	return (new);
 }
